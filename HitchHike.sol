2ffce5e2239b85be55fc4e3656bb6c5f527b8acc
Dan-Dazzell <bennyjaheim@gmail.com>
2023-10-10 00:14:48 -0400

    Ye fr

--- HitchHike.sol
+++ HitchHike.sol
@@ -1,8 +1,24 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HitchHike {
-
-    function pay() public payable {
-         
+contract HitchHike { 
+    struct RideRequest {
+        address hitchhiker;
+        string currentLocation;
+        string destination;
+        uint256 cost;
+        uint256 estimatedTime;
+        bool isAccepted;
+        bool isCompleted;
     }
+
+    struct RideDetails {
+        address driver;
+        string vehicleType;
+        string currentLocation;
+        uint256 cost;
+        uint256 estimatedTime;
+    }
+
+    
 }
